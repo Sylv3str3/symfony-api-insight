a3a26ba1feeb1bb82fecdccb13e09c2cecc84e77
Sylvestre <139703387+Sylv3str3@users.noreply.github.com>
2025-06-04 07:41:41 +0300

    fix: bundle compatibility with symfony bundle extension interface

--- src/ApiInsight/ApiInsightBundle.php
+++ src/ApiInsight/ApiInsightBundle.php
@@ -4,11 +4,12 @@ namespace ApiInsight;
 
 use ApiInsight\DependencyInjection\ApiInsightExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class ApiInsightBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new ApiInsightExtension();
     }
-} 
+}
